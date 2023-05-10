b2be1cbca3e656b1ab1a67b274a5168f18b5fa6f
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-10 21:04:03 +0100

    Modify all files

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,9 @@ import datetime
 class BaseModel:
     def __init__(self):
         self.id = uuid.uuid4()
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        print(f"ID: {self.id} CREATED ON: {self.created_at} UPDATED ON: {self.updated_at}")
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
+        print(f"CREATED: {self.created_at} UPDATED: {self.updated_at}")
 
     def __str__(self):
         return f"{self.__class__.__name__} {self.id} {self.__dict__}"
@@ -19,5 +19,5 @@ class BaseModel:
     def to_dict(self):
         self.__dict__["__class__"] = self.__class__.__name__
         return f"{self.__dict__}"
+    
 first_instance = BaseModel()
-print(first_instance.to_dict())
