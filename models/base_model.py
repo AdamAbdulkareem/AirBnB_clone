d058d4697384cfe0dd5d7fc6a09fe2900a4aa9c9
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-10 20:16:53 +0100

    Modify all files

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-class BaseModel:
-    def __init__(self):
 
+import uuid
+from datetime import datetime
+class BaseModel:
+    def __init__(self, id, created_at, updated_at):
+        self.id = uuid.uuid4()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.id} {self.__dict__}"
