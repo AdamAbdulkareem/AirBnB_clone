9d42911f9ffcd2728f4be25b48f32e035c9ebda1
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-11 13:05:04 +0100

    Modify all files

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,13 @@ class BaseModel:
         self.id = uuid.uuid4()
         self.created_at = datetime.datetime.now().isoformat()
         self.updated_at = datetime.datetime.now().isoformat()
-        print(f"CREATED: {self.created_at} UPDATED: {self.updated_at}")
 
     def __str__(self):
         return f"{self.__class__.__name__} {self.id} {self.__dict__}"
+
     def save(self):
         self.updated_at = datetime.datetime.now()
-        
+    
     def to_dict(self):
         self.__dict__["__class__"] = self.__class__.__name__
-        return f"{self.__dict__}"
-    
-first_instance = BaseModel()
+        return self.__dict__
