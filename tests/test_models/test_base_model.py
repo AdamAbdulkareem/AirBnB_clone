c8cf4cf3c7aad09bf84516c0f452eda61166fcd8
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-11 17:25:27 +0100

    Modify all files

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import unittest
+import datetime
+import models
+from models.base_model import BaseModel
+
+
+class testBaseModel(unittest.TestCase):
+
+    def test_created_at(self):
+        my_instance = BaseModel()
+        result = my_instance.created_at
+        excepted_result = datetime.datetime.now().isoformat()
+        self.assertEqual(result, excepted_result)
+
+    def test_updated_at(self):
+        my_instance = BaseModel()
+        my_instance.save()
+        result = my_instance.updated_at
+        excepted_result = datetime.datetime.now()
+        self.assertEqual(result, excepted_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
