a0ea519704d2677294f1f99e78a9797ee12056aa
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-12 17:12:41 +0100

    Modify all files

---
+++ models/engine/file_storage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+This module converts the dictionary representation to a JSON string
+"""
+
+class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
