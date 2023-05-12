2b3a331449db3f437314f006ef58703b80dd4d7c
adamabdulkareem <dmabdulkareem@gmail.com>
2023-05-12 04:23:15 -0700

    Modify all files

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
         else:
-            self.id = uuid.uuid4()
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now().isoformat()
             self.updated_at = datetime.datetime.now().isoformat()
 
