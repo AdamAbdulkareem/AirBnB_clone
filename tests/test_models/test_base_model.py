b73d0d6cc5b0b80c919c50613d6d738c047a0f05
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-12 11:32:59 +0100

    Modify all files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,8 +10,9 @@ class testBaseModel(unittest.TestCase):
     def test_created_at(self):
         my_instance = BaseModel()
         result = my_instance.created_at
-        excepted_result = datetime.datetime.now().isoformat()
+        excepted_result = datetime.datetime.now()
         self.assertEqual(result, excepted_result)
+        self.assertIs(type(result), datetime.datetime)
 
     def test_updated_at(self):
         my_instance = BaseModel()
@@ -19,11 +20,18 @@ class testBaseModel(unittest.TestCase):
         result = my_instance.updated_at
         excepted_result = datetime.datetime.now()
         self.assertEqual(result, excepted_result)
+        self.assertIs(type(result), datetime.datetime)
+        
     def test_to_dict(self):
         my_instance = BaseModel()
         result = my_instance.to_dict()
         expected_result = my_instance.__dict__
         self.assertEqual(result, expected_result)
+        
+    def test___init__(self):
+        my_instance = BaseModel(first_name="Adam", last_name="Abdulkareem")
+        self.assertEqual(my_instance.first_name, "Adam")
+        self.assertEqual(my_instance.last_name, "Abdulkareem")
 
 if __name__ == '__main__':
     unittest.main()
