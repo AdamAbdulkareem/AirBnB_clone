c7595b58fa8e67283f7c0c1330664aeaa85d3861
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-12 11:17:43 +0100

    Modify all files

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,8 @@ class BaseModel:
         self.updated_at = datetime.datetime.strptime(
             self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
         for key, value in kwargs.items():
-            setattr(self, key, value)
+            if key != '__class__':
+                setattr(self, key, value)
 
     def __str__(self):
         """Returns the object representation in a string format."""
