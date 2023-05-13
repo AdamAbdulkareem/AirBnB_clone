bd9413eaef63938bd9f51224033224578f3e4b7b
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-13 15:11:43 +0100

    Formatted base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ This module defines all common attributes or methods for other classes
 import uuid
 import datetime
 
+
 class BaseModel:
     """Base Class"""
 
@@ -13,16 +14,15 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at":
-                    value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key == "updated_at":
-                    value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now().isoformat()
             self.updated_at = datetime.datetime.now().isoformat()
-            
 
     def __str__(self):
         """Returns the object representation in a string format."""
