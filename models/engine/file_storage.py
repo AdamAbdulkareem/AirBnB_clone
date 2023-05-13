f9cc076deb2a0d810029b7c5add1b0581125b412
adamabdulkareem <dmabdulkareem@gmail.com>
2023-05-13 09:32:29 -0700

    Add executable to files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,19 +37,19 @@ class FileStorage:
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
 
-        ''' create empty dictionary'''
+        """ create empty dictionary"""
         json_object = {}
         """ fill dictionary with elements __objects """
         for key in self.__objects:
             json_object[key] = self.__objects[key].to_dict()
 
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             json.dump(json_object, f)
 
     def reload(self):
-        """ deserializes the JSON file to __objects """
+        """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r', encoding="UTF8") as f:
+            with open(self.__file_path, "r", encoding="UTF8") as f:
                 # jlo = json.load(f)
                 for key, value in json.load(f).items():
                     attri_value = eval(value["__class__"])(**value)
