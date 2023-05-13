86ab00340a827173ad14999a2bd96677428b806c
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-13 15:13:43 +0100

    Formatted all .py extension files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,7 +6,6 @@ from models.base_model import BaseModel
 
 
 class testBaseModel(unittest.TestCase):
-
     def test_created_at(self):
         my_instance = BaseModel()
         result = my_instance.created_at
@@ -21,17 +20,18 @@ class testBaseModel(unittest.TestCase):
         excepted_result = datetime.datetime.now()
         self.assertEqual(result, excepted_result)
         self.assertIs(type(result), datetime.datetime)
-        
+
     def test_to_dict(self):
         my_instance = BaseModel()
         result = my_instance.to_dict()
         expected_result = my_instance.__dict__
         self.assertEqual(result, expected_result)
-        
+
     def test___init__(self):
         my_instance = BaseModel(first_name="Adam", last_name="Abdulkareem")
         self.assertEqual(my_instance.first_name, "Adam")
         self.assertEqual(my_instance.last_name, "Abdulkareem")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
