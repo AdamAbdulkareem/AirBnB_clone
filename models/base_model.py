02ce7ed330c155fda1f6d36b43ffe6999d575961
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-14 15:38:23 +0100

    Modify file_storage.py and __init__.py files

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines all common attributes or methods for other classes"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """Base Class"""
@@ -20,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return [<class name>] (<self.id>) <self.__dict__>"""
@@ -28,6 +29,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+"""This module convert the dictionary representation to a JSON string"""
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """Serializes instances to a JSON file and Deserializes JSON file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = obj.__class__.__name__ + "." + str(obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        json_object = {}
+
+        for key, value in self.__objects.items():
+            json_object[key] = value.to_dict()
+
+        with open(self.__file_path, mode="w", encoding="UTF8") as json_file:
+            json.dump(json_object, json_file)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects """
+        try:
+            with open(self.__file_path, 'r', encoding="UTF8") as f:
+                # jlo = json.load(f)
+                for key, value in json.load(f).items():
+                    attri_value = eval(value["__class__"])(**value)
+                    self.__objects[key] = attri_value
+        except FileNotFoundError:
+            pass
