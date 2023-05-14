19df792aa8c20464c54d4211793110821ef9dca0
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-14 12:04:56 +0100

    Update base_model.py with *args, **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ from datetime import datetime
 class BaseModel:
     """Base Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiate class attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return [<class name>] (<self.id>) <self.__dict__>"""
