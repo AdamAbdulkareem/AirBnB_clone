4466693cd183c03cfeb284ba6140908d22d8bcd2
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-15 12:10:09 +0100

    Modify console.py file

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import cmd
+"""This module contains the entry point of the command interpreter"""
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+    
+    def emptyline(self):
+        """Do nothing on an empty line."""
+        pass
+    
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+    
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_help(self, arg):
+        """Help command to display help"""
+        cmd.Cmd.do_help(self, arg)
+        
+        
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
