19b3fc09308e0399ad34db2c205bd716f23b269d
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-05-16 17:20:47 +0100

    Modify user.py file

--- models/user.py
+++ models/user.py
@@ -2,12 +2,9 @@
 """This is the module for user"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    def __init__(self, email: str, password: str, first_name: str, last_name: str):
+    def __init__(self):
         """Instantiate the class"""
         """Inherit all methods and properties from its parent"""
         super().__init__()
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
